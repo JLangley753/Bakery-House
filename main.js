db388cd54b4a1b3f7316db0fabdfa0a39d6faeb5
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-12-02 10:02:26 +0000

    Added special cards with images.
    Amended carousel mechanism

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Burger menu opening mechanism
+
 var sidemenu = document.querySelector('.sidemenu');
 var dots = document.getElementsByClassName('dot');
 var dotsArray = [].slice.call(dots);
@@ -10,4 +12,40 @@ function showmenu() {
   for (var i = 0; i < dotsArray.length; i++) {
     dotsArray[i].classList.toggle('changedot' + [i + 1]);
   }
+}
+
+// Figuring width of special box to determine width of special boxes and translation
+
+var spec = document.querySelectorAll('.spec');
+var specgridwrap = document.querySelector('.specialgridwrap');
+
+changespecialgridsize();
+
+window.onresize = changespecialgridsize;
+
+function changespecialgridsize(event) {
+  for (var i = 0; i < spec.length; i++) {
+    spec[i].style.width = specgridwrap.offsetWidth * 0.5 + 'px';
+  }
+}
+
+var leftarrow = document.querySelector('.leftarrow');
+var rightarrow = document.querySelector('.rightarrow');
+var carousel = document.querySelector('.topspecialscarousel');
+
+leftarrow.onclick = carouselleft;
+rightarrow.onclick = carouselright;
+
+function carouselleft() {
+  var currentTranslate = parseInt(carousel.style.transform.match((/-?\d+/) || []));
+  console.log('cl: ' + currentTranslate);
+  var transformleft = currentTranslate + specgridwrap.offsetWidth * 0.5;
+  carousel.style.transform = 'translate(' + transformleft + 'px)';
+}
+
+function carouselright() {
+  var currentTranslate = parseInt(carousel.style.transform.match((/-?\d+/) || []));
+  console.log('cr: ' + currentTranslate);
+  var transformright = currentTranslate - specgridwrap.offsetWidth * 0.5;
+  carousel.style.transform = 'translate(' + transformright + 'px)';
 }
